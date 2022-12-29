c8e350f33c06a0162a53c1706659f12e0ca6c255
Krzysztof Romanowski <krzysztof.romanowski@zendesk.com>
2022-12-29 22:30:24 +0100

    Improve docker compose run script

--- run-compose-dev.sh
+++ run-compose-dev.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d
+docker-compose rm --all
+docker-compose pull
+docker-compose build --no-cache
+docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d --force-recreate
