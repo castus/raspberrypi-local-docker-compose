7058cac7929616d864dacb3a51f3dd746fe053f5
Krzysztof Romanowski <krzysztof.romanowski@zendesk.com>
2023-04-11 19:57:46 +0200

    Remove Ble cron container

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-docker compose rm --all
-docker compose pull
-docker compose build --no-cache
-docker compose -f docker-compose.yml -f docker-compose-prod.yml up -d --force-recreate
+docker compose -f docker-compose.yml -f docker-compose-prod.yml up -d
